07f492dad4af3f8abd7c59d3f1e7e689806f2038
Martin Zachrison <zac@cyberzac.se>
2018-05-16 21:11:53 +0200

    Passes tests

--- src/test/scala/cryptic/CaesarSpec.scala
+++ src/test/scala/cryptic/CaesarSpec.scala
@@ -5,35 +5,45 @@ import org.scalatest._
 
 class CaesarSpec extends FlatSpec with Matchers {
 
+  val key1 = Caesar.Key(1)
+
   "Caesar Encryptor" should "encrypt to shifted string" in {
-    Caesar.encryptor(Caesar.Key(0)).encrypt(PlainText("kalle")) shouldEqual CipherText("kalle".getBytes())
-    Caesar.encryptor(Caesar.Key(1)).encrypt(PlainText("kalle")) shouldEqual CipherText("lbmmf".getBytes())
+    Caesar.encryptor(key1).encrypt(PlainText("kalle")) shouldEqual CipherText("lbmmf".getBytes())
   }
   "Caesar Decryptor" should "decrypt to shifted string" in {
-    Caesar.decryptor(Caesar.Key(0)).decrypt(CipherText("kalle".getBytes())) map(_.toList)  shouldEqual Right(PlainText("kalle").toList)
-    Caesar.decryptor(Caesar.Key(1)).decrypt(CipherText("lbmmf".getBytes())) map(_.toList) shouldEqual Right(PlainText("kalle").toList)
+    Caesar.decryptor(key1).decrypt(CipherText("lbmmf".getBytes())) map (_.toList) shouldEqual Right(PlainText("kalle").toList)
   }
 
   "Caesar Encrypted" should "support encryption and decryption" in {
-    val key = Caesar.Key(0)
-    implicit val ce: Encryptor = Caesar.encryptor(key)
-    val e = Encrypted[String]("nisse")
-    e match {
-      case Encrypted.Value(ct) ⇒ ct.contains("nisse") shouldBe false // Verify that cipher text somewhat hides plain text.
-      case _ ⇒ fail("does not decrypt")
+    implicit val decryptor: Decryptor = Caesar.decryptor(key1)
+    encrypt(key1, "nisse").decrypted match {
+      case Right(decrypted) ⇒ decrypted shouldEqual "nisse"
+      case x ⇒ fail(s"does not decrypt: $x")
     }
+  }
 
-    implicit val cd: Decryptor = Caesar.decryptor(key)
-    val decrypted: Either[String, String] = e.decrypted
-    decrypted.contains("nisse") shouldBe true
+  "Caesar Encrypted" should "hide plaintext" in {
+    encrypt(key1, "nisse") match {
+      case Encrypted.Value(ct) ⇒ new String(ct).contains("nisse")
+      case _ ⇒ None
+    }
+  }
+
+  "Caesar zero" should "not be valid" in {
+   assertThrows[IllegalArgumentException] { Caesar.Key(0) }
+  }
+
+  private def encrypt(key: Caesar.Key, plain: String) = {
+    implicit val ce: Encryptor = Caesar.encryptor(key)
+    Encrypted[String](plain)
   }
   /*
   def createUser: User = {
-    implicit val key: Caesar.Key = Cryptos.Caesar.Key(1)
+    implicit val key: Caesar.Key = Cryptos.key1
     User(1, "kalle", Encrypted(PersonName(first = Name("Karl") ,last = Name("Nilsson"))), Encrypted(EmailAddress("kalle@nilsson.se")))
   }
   "Caesar crypto" should "work when used with correct key" in {
-    implicit val key: Caesar.Key = Cryptos.Caesar.Key(1)
+    implicit val key: Caesar.Key = Cryptos.key1
     val user = createUser
     user.email.map(_.literal).decrypted shouldEqual "kalle@nilsson.se"
   }

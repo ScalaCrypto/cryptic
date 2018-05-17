9eb919f55d23e1e0b3d379cab98a1b6ffd41c0a5
Odd Möller <odd.moller@gmail.com>
2018-05-17 13:04:35 +0200

    Remove old code.

--- src/main/scala/cryptic/serialization/FstSerializer.scala
+++ src/main/scala/cryptic/serialization/FstSerializer.scala
@@ -9,8 +9,4 @@ object FstSerializer {
     override def serialize(value: V) = PlainText(fst.asByteArray(value))
     override def deserialize(plainText: PlainText) = Right(fst.asObject(plainText).asInstanceOf[V])
   }
-  /*
-  implicit val serializer: Serializer[Any] = (value: Any) => PlainText(fst.asByteArray(value))
-  implicit val deserializer: Deserializer[Any] = (plainText: PlainText) => Right(fst.asObject(plainText))
-  */
 }

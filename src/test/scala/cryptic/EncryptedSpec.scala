c7b698df437c7f13078c6adb5936a60fca43e3fa
Odd Möller <odd.moller@gmail.com>
2018-05-17 13:24:38 +0200

    Add test case for case class serialization with FST.

--- src/test/scala/cryptic/EncryptedSpec.scala
+++ src/test/scala/cryptic/EncryptedSpec.scala
@@ -4,30 +4,4 @@ import org.scalatest._
 import PlainText._
 
 class EncryptedSpec extends FlatSpec with Matchers {
-  case class Name(literal: String)
-  case class PersonName(first: Name, middle: Option[Name] = None, last: Name)
-  case class EmailAddress(literal: String)
-
-  case class User(
-    id: Long,
-    alias: String,
-    name: Encrypted[PersonName],
-    email: Encrypted[EmailAddress])
-
-  /*
-  def createUser: User = {
-    implicit val key: Ceasar.Key = Cryptos.Ceasar.Key(1)
-    User(1, "kalle", Encrypted(PersonName(first = Name("Karl") ,last = Name("Nilsson"))), Encrypted(EmailAddress("kalle@nilsson.se")))
-  }
-  "Ceasar crypto" should "work when used with correct key" in {
-    implicit val key: Ceasar.Key = Cryptos.Ceasar.Key(1)
-    val user = createUser
-    user.email.map(_.literal).decrypted shouldEqual "kalle@nilsson.se"
-  }
-  "Ceasar crypto" should "not work when used with incorrect key" in {
-    implicit val key: Ceasar.Key = Cryptos.Ceasar.Key(2)
-    val user = createUser
-    user.email.map(_.literal).decrypted shouldEqual "kalle@nilsson.se"
-  }
-  */
 }
--- src/test/scala/cryptic/serialization/FstSerializerSpec.scala
+++ src/test/scala/cryptic/serialization/FstSerializerSpec.scala
@@ -1,15 +1,30 @@
 package cryptic
 package serialization
 
+import cryptic.crypto.Caesar
 import org.scalatest._
+
+
+case class Name(literal: String)
+case class PersonName(first: Name, middle: Option[Name] = None, last: Name)
+case class EmailAddress(literal: String)
+
+case class User(id: Long, alias: String, name: PersonName, email: EmailAddress)
 
 class FstSerializerSpec extends FlatSpec with Matchers {
   import FstSerializer._
 
-  "Fst serializer" should "serialize then deserialize back to original" in {
+  "Fst serializer" should "serialize string and then deserialize back to original string" in {
     val pt = serializer.serialize("kalle")
     pt shouldNot equal(PlainText("kalle"))
     val o = serializer.deserialize(pt)
     o shouldEqual Right("kalle")
+  }
+
+  "Fst serializer" should "serialize user and deserialize back to original user" in {
+    val user = User(1, "kalle", PersonName(first = Name("Karl") ,last = Name("Nilsson")), EmailAddress("kalle@nilsson.se"))
+    val pt = serializer.serialize(user)
+    val o = serializer.deserialize(pt)
+    o shouldEqual Right(user)
   }
 }

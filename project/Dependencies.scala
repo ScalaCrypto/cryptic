768c05bd079546dddbbfda160875605e74f6d24b
Odd Möller <odd.moller@gmail.com>
2018-06-13 16:35:29 +0100

    Upgrade ScalaTest to 3.0.5.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,6 +1,6 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.4"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5" withSources()
   lazy val fst = "de.ruedigermoeller" % "fst" % "2.56" withSources()
 }

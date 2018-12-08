712fdb9c84c211b966236df63b6372547a268d3a
Martin Zachrison <zac@cyberzac.se>
2018-12-08 22:19:00 +0100

    Feature/ecdsa (#8)
    * Enabled travis
    
    * Implemented Elliptic Curve.

---
+++ crypto-bouncycastle/src/test/scala/cryptic/crypto/ECIESSpec.scala
@@ -0,0 +1,35 @@
+package cryptic.crypto
+
+import java.security.{KeyPair, PrivateKey, PublicKey}
+
+import cryptic.{CipherText, Decrypt, Encrypt, PlainText}
+import org.scalatest._
+
+class ECIESSpec extends FlatSpec with Matchers {
+  import ECIES._
+  private val keyPair: KeyPair = keygen(256)
+  implicit val publicKey: PublicKey = keyPair.getPublic
+  private val text = "nisse"
+  private val plainText = PlainText(text)
+  val encryptFun: Encrypt = encrypt // Uses implicit key
+
+  "ECIES" should "support encryption and decryption" in {
+    // Note no need for the private key when encrypting
+    val encrypted = encryptFun(plainText)
+
+    implicit val privateKey: PrivateKey = keyPair.getPrivate
+    val decryptFun: Decrypt = decrypt // Uses implicit key
+    decryptFun(encrypted) match {
+      case Right(actual) ⇒ actual shouldEqual plainText
+      case x ⇒ fail(s"does not decrypt: $x")
+    }
+  }
+
+  "ECIES" should "hide plaintext" in {
+    encryptFun(plainText) match {
+      case ct: CipherText ⇒ new String(ct.bytes).contains("nisse".getBytes())
+      case _ ⇒ fail(s"""could not encrypt "$text"""")
+    }
+  }
+
+}

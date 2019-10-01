6fb125837f602a5c6c9b9edc9ee9578405401a04
Odd Möller <odd.moller@gmail.com>
2019-10-01 14:23:42 +0200

    Upgrades all dependencies (#12)

--- project/Dependencies.scala
+++
@@ -1,6 +0,0 @@
-import sbt._
-
-object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.8" withSources()
-  lazy val fst = "de.ruedigermoeller" % "fst" % "2.57" withSources()
-}
